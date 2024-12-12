c006ceda87ac4e0cc60c78aa8de58cb20ccf79f1
haller125 <erokkabash1@gmail.com>
2024-12-12 19:08:19 +0500

    Launch script

--- launch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Start Vite in a new command prompt
-cd /d C:\Users\qwerty\WebstormProjects\3dkanoodle\Views
-start cmd /k "npx vite"
-
-:: Activate the virtual environment and run Django in a new command prompt
-cd /d C:\Users\qwerty\WebstormProjects\3dkanoodle
-start cmd /k "venv\Scripts\activate && python manage.py runserver"
