22e9dda97cb817766171edceb6d66be27af1654d
Erfan Ghoreishi <erfan@Erfans-MacBook-Air-3.local>
2024-12-12 09:43:05 +0000

    api find solutions

--- solver/urls.py
+++ solver/urls.py
@@ -3,9 +3,13 @@ from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 from . import views3d
+from . import solver3d
+
 
 urlpatterns = [
     path("index", views.index, name="index"),
     path("submit/", views.submit, name="submit"),
-    path("3d/submit/", views3d.submit, name="3dsubmit"),
+    path("3d/submit/", solver3d.submit, name="3dsubmit"),
+
+    #path("3d/submit/", views3d.submit, name="3dsubmit"),
 ]
